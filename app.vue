94d910a8d97deb7bc436fd01bae4438241fafd3d
Erin P <91641692+3rinnn72@users.noreply.github.com>
2024-10-31 16:27:26 -0400

    app.vue
    added app.vue and the standard code from the tutorial https://vuejs.org/guide/quick-start

---
+++ app.vue
@@ -0,0 +1,15 @@
+<script>
+export default {
+  data() {
+    return {
+      msg: 'Hello World!'
+    }
+  }
+}
+</script>
+
+<template>
+  <h1>{{ msg }}</h1>
+  <input v-model="msg">
+</template>
+
