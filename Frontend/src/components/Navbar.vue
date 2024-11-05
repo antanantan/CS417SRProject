07ab4e46483225ec28425c995afbcf83ac1ab193
antanantan <cqclr127@gmail.com>
2024-11-04 21:21:09 -0500

    WIP - Implementing LoginView

--- Frontend/src/components/Navbar.vue
+++ Frontend/src/components/Navbar.vue
@@ -17,9 +17,9 @@ const isActiveLink = (routePath) => {
         >
           <!-- Logo -->
           <RouterLink class="flex flex-shrink-0 items-center mr-4" to="/">
-            <img class="h-10 w-auto" :src="logo" alt="Vue Jobs" />
+            <img class="h-10 w-auto" :src="logo" alt="Eat Well" />
             <span class="hidden md:block text-white text-2xl font-bold ml-2"
-              >Vue Jobs</span
+              >Eat Well</span
             >
           </RouterLink>
           <div class="md:ml-auto">
---
+++ Frontend/src/views/HomeView.vue
@@ -0,0 +1,9 @@
+<script setup>
+import HomeCards from '@/components/HomeCards.vue';
+</script>
+
+<template>
+  <Hero />
+  <HomeCards />
+  <JobListings :limit="3" :showButton="true" />
+</template>
