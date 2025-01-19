ea3091e96bc10f42cb1166f3febff9942df3048f
Erin P <91641692+3rinnn72@users.noreply.github.com>
2025-01-19 09:50:21 -0500

    Map WIP
    I got the map to display, now we need to work on getting the site to accept a specific location

--- Backend/map.py
+++ Backend/map.py
@@ -9,5 +9,5 @@ m = folium.Map(location=[40.7128, -74.0060], zoom_start=12)
 folium.Marker([40.7128, -74.0060], popup="New York City").add_to(m)
 
 # Save the map as an HTML file
-m.save("/Frontend/src/assets/map.html")
+m.save("../Frontend/src/assets/map.html")
 # i might be trippin but how do you have it save in a certain folder
